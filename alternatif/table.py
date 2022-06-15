3787e387743fc12722d0f5f1cc9bb017c9d6e21e
evrydays <dimasmaehendran@gmail.com>
2022-06-15 16:34:32 +0700

    minor adit

--- alternatif/table.py
+++ alternatif/table.py
@@ -9,7 +9,7 @@ class formtabel(forms.ModelForm):
             'kodealternatif': forms.TextInput(attrs={'class':'form-control border focus:border-sky-500 rounded-[4px] w-1/4 h-[2.5rem] px-[.5rem] mt-[.5rem]', 'placeholder':'KODE ALTERNATIF'}),
             'namaalternatif': forms.TextInput(attrs={'class':'form-control border focus:border-sky-500 rounded-[4px] w-1/4 h-[2.5rem] px-[.5rem] mt-[.5rem]', 'placeholder':'NAMA ALTERNATIF'}),
             'c1': forms.TextInput(attrs={'class':'form-control border focus:border-sky-500 rounded-[4px] w-3/4 h-[2.5rem] px-[.5rem] mt-[.5rem]', 'placeholder':'JUTA Rp.', 'type':"number", }),
-            'c2': forms.TextInput(attrs={'class':'form-control border focus:border-sky-500 rounded-[4px] w-3/4 h-[2.5rem] px-[.5rem] mt-[.5rem]', 'placeholder':'%'}),
+            'c2': forms.TextInput(attrs={'class':'form-control border focus:border-sky-500 rounded-[4px] w-3/4 h-[2.5rem] px-[.5rem] mt-[.5rem]', 'placeholder':'prosentase (%)'}),
             'c3': forms.Select(attrs={'class':'form-control border focus:border-sky-500 rounded-[4px] w-3/4 h-[2.5rem] px-[.5rem] mt-[.5rem]', 'placeholder':'C3'}),
             'c4': forms.Select(attrs={'class':'form-control border focus:border-sky-500 rounded-[4px] w-3/4 h-[2.5rem] px-[.5rem] mt-[.5rem]', 'placeholder':'C4'}),
             'c5': forms.Select(attrs={'class':'form-control border focus:border-sky-500 rounded-[4px] w-3/4 h-[2.5rem] px-[.5rem] mt-[.5rem]', 'placeholder':'C5'}),            
